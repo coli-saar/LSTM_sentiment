5a120f6bc892e4fcf7511308fd095caa2eff255c
Alexander Koller <akoller@gmail.com>
2018-04-17 18:28:14 +0200

    works on jones

--- settings.py
+++ settings.py
@@ -19,6 +19,9 @@ LEARNING_RATE = 0.001
 BATCH_SIZE = 100
 GPU = torch.cuda.is_available()
 
+print("available")
+print(GPU)
+
 MODEL = {
     "model": models.PureGRU,
     "model_name": "PureGRU",
@@ -40,6 +43,10 @@ VISUALIZE = args.visualize
 CHECKPOINT_DIR = "checkpoints"
 
 GPU = torch.cuda.is_available() and args.enable_cuda
+
+print("enable")
+print(args.enable_cuda)
+
 HIST_OPTS = dict(numbins=20,
                  xtickmin=0,
                  xtickmax=6)
